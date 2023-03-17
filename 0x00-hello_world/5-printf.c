05249b54065867abe43ad8a9e6512c9dfa743328
Ghaleeb24 <galibsonumar@gmail.com>
2023-03-17 11:56:04 -0700

    Hello, puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - this is the main function that describe the input of a program
+ * main - this is the main function
  *
- * Return: 0 when successfull
+ * Return: 0 when successfull 
  */
 
-int maain(void)
+in main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
